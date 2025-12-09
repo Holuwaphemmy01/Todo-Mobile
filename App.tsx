87893eb6b64c707179caf0ed81ec7896b35523f4
Holuwaphemmy01 <jacoboluwafemi2002@gmail.com>
2025-12-09 14:21:14 +0100

    App update

--- App.tsx
+++ App.tsx
@@ -2,13 +2,23 @@ import React from 'react'
 import { SafeAreaView } from 'react-native'
 import AppNavigator from './src/navigation'
 import { TaskProvider } from './src/store/tasks'
+import { ThemeProvider, useTheme } from './src/theme'
+
+function Root() {
+  const { theme } = useTheme()
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: theme.colors.bg }}>
+      <AppNavigator />
+    </SafeAreaView>
+  )
+}
 
 export default function App() {
   return (
-    <TaskProvider>
-      <SafeAreaView style={{ flex: 1 }}>
-        <AppNavigator />
-      </SafeAreaView>
-    </TaskProvider>
+    <ThemeProvider>
+      <TaskProvider>
+        <Root />
+      </TaskProvider>
+    </ThemeProvider>
   )
 }
